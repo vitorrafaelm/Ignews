41b730871124b99532660356ad2d840e8f9d360d
vitorrafaelm <vitor.rafael1518@gmail.com>
2021-08-28 13:27:15 -0300

    Working with static site generation and server side rendering and stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,61 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import styles from "./home.module.scss";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string; 
+    amount: number; 
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
+
+// Modelo exato a seguir
+// O getServerSideProps é executado na camada no next e não no browser
+// Todo o retorno em props: {} pode ser acessado pelo componente que é
+// renderizado no navegador;
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JTTPxEjQZ51vuFxkInn6LXY");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency', 
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  };
+};
