5d232455f8f044448bb1ccf694452ee26bbf2d4c
vitorrafaelm <vitor.rafael1518@gmail.com>
2021-10-16 12:02:50 -0300

    stripe and fauna db - subscription done

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,41 @@
+import { useSession, signIn } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stipe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string,
+  priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    // Criação da checkout session
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs(); 
+
+      await stripe.redirectToCheckout({ sessionId }); 
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,14 +1,53 @@
+import { query as q } from 'faunadb'; 
+
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user; 
+      
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'), 
+                  q.Casefold(user.email)
+                )
+              )
+            ), 
+            q.Create(
+              q.Collection('user'), 
+              { data: { email }},
+            ), 
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'), 
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true;
+
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+      
+      
+    },
+  },
 });
